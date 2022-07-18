fcd2ed787fef768531d0ca0875298857d903e9b0
Hyuna Park <hyuna_park@adenasoft.com>
2022-07-18 15:41:37 +0900

    feat: global style 및 layout 설정

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,31 @@
 // default는 반드시 포함되어야 함.
 // url: http://localhost:3000/posts/first-post
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
+// import Script from 'next/script';
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        {/** 브라우저 tab 위에 생기는 이름 */}
+        <title>첫번째 포스팅</title>
+      </Head>
+      {/* <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
