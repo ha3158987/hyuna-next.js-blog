e78221da7cdb94efe27673dfe23084a825a855a7
Hyuna Park <hyuna_park@adenasoft.com>
2022-07-18 15:45:56 +0900

    fix: Home 페이지 레이아웃 수정

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Hyuna';
+const name = 'Dico';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
