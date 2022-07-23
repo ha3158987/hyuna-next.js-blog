7250cd26f03b48b28b5d423202782b19e4ee9796
Hyuna Park <hyuna_park@adenasoft.com>
2022-07-23 23:33:48 +0900

    feat: remark 라이브러리 설치, 포스팅 본문 컨텐츠 렌더링

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,13 @@
 //dynamic routes
+import Head from 'next/head';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
-      clg: params
     },
   };
 }
@@ -20,15 +20,18 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({ postData, clg }) {
-  console.log(clg)
+export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
