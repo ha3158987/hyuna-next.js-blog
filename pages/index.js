483509d86a34fe3d7900f562a2bf4339848ef026
Hyuna Park <hyuna_park@adenasoft.com>
2023-09-17 21:52:02 +0900

    feat: emotion 설치, 메인 desc 수정

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Date from '../components/date';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import React from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import Date from "../components/date";
+import Layout from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import React from "react";
+import styled from "@emotion/styled";
 
 // server-side에서만 동작 (JS 번들링에 포함되지 않음)
 // getStaticProps: page디렉토리 안에서만 실행
@@ -17,14 +18,16 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{"Dico's Log"}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>안녕하세요, 소소하게 오래 개발하고 싶은 개발자 Dico 입니다.</p>
+        <MainDescription>
+          안녕하세요, 소소하게 오래 개발하고 싶은 Dico 입니다.
+        </MainDescription>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Posts</h2>
@@ -48,3 +51,10 @@ export default function Home({allPostsData}) {
     </Layout>
   );
 }
+
+const MainDescription = styled.div`
+  color: #888888;
+  font-size: 18px;
+  text-align: center;
+  margin-bottom: 30px;
+`;
